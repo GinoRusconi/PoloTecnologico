2a2c3fe89dded4a04e2c250c91c18a8d124051db
Gino Rusconi <ginoblax@gmail.com>
2022-06-08 16:24:15 -0300

    Inicio

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,29 @@
+﻿bool continuar = true;
+
+while (continuar)
+{
+    Console.Clear();
+    Console.WriteLine("Hola, introduzca su nombre");
+
+    String nombrePerson = Console.ReadLine();
+    Console.WriteLine($"¡Hola {nombrePerson}!");
+
+    Console.WriteLine("Desea continuar? Si=S, No=N");
+    String opcionContinuar = Console.ReadLine();
+
+    if (opcionContinuar != null)
+    {
+        opcionContinuar = opcionContinuar.ToUpper();
+        if (opcionContinuar == "N")
+        {
+            continuar = false;
+        }
+        else if (opcionContinuar != "S")
+        {
+            continuar = false;
+            Console.WriteLine("Opcion no valida");
+        }
+    }
+}
+Console.WriteLine("Programa finalizado");
+Console.ReadLine();
