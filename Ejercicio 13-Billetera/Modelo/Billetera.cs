54fbb1a62710ab018b3b171e96240efe714dbb18
Gino Rusconi <ginoblax@gmail.com>
2022-06-08 20:30:46 -0300

    Inicio

---
+++ Ejercicio 13-Billetera/Modelo/Billetera.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_13_Billetera.Modelo
+{
+    internal class Billetera
+    {
+        private int[,] _billeteraVirtual = new int[2, 7];
+
+        public int iD { get; set; }
+        public int BilletesDe10 
+        {
+            set 
+            { 
+                _billeteraVirtual[0,0] = value;
+            }
+            
+        }
+        public int BilletesDe20
+        {
+            set
+            {
+                _billeteraVirtual[0,1] = value;
+                _billeteraVirtual[1,1] = 20;
+            }
+
+        }
+        public int BilletesDe50
+        {
+            set
+            {
+                _billeteraVirtual[0,2] = value;
+                _billeteraVirtual[1,2] = 50;
+            }
+
+        }
+        public int BilletesDe100
+        {
+            set
+            {
+                _billeteraVirtual[0,3] = value;
+                _billeteraVirtual[1,3] = 100;
+            }
+
+        }
+        public int BilletesDe200
+        {
+            set
+            {
+                _billeteraVirtual[0,4] = value;
+                _billeteraVirtual[1,4] = 200;
+            }
+
+        }
+        public int BilletesDe500
+        {
+            set
+            {
+                _billeteraVirtual[0,5] = value;
+                _billeteraVirtual[1,5] = 500;
+            }
+
+        }
+        public int BilletesDe1000
+        {
+            set
+            {
+                _billeteraVirtual[0,6] = value;
+                _billeteraVirtual[1,6] = 1000;
+            }
+
+        }
+
+        //Sumar billetes y devolver total
+        public double Total()
+        {
+            double total=0;
+            for (int i = 0; i < 7; i++)
+            {
+                total += _billeteraVirtual[0,i] * _billeteraVirtual[1,i];
+            }
+            return total;
+        }
+
+        // recibir una billetera y conbinar
+        public Billetera Combinar(Billetera billetera2)
+        {
+            
+            Billetera billeteraNueva = new Billetera();
+
+            for (int i = 0; i < 7; i++)
+            {
+                billeteraNueva._billeteraVirtual[0,i] = _billeteraVirtual[0,i] + billetera2._billeteraVirtual[0,i];
+                billeteraNueva._billeteraVirtual[1, i] = _billeteraVirtual[1, i];
+            }
+            
+            return billeteraNueva;
+        }
+
+        public void MostrarContenidoDeBilletera()
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"El contenido de la billetera {iD} es:");
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Billetes De {_billeteraVirtual[1,i]}: {_billeteraVirtual[0,i]}");
+            }
+        }
+
+    }
+
+    
+}
--- Ejercicio 13-Billetera/Program.cs
+++ Ejercicio 13-Billetera/Program.cs
@@ -1,1 +1,86 @@
-﻿
+﻿using Ejercicio_13_Billetera.Modelo;
+
+List<Billetera> billeteras = new List<Billetera>();
+
+do
+{
+    int opcion;
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Bienvenido a la administracion de billeteras, que accion desea hacer?");
+        Console.WriteLine("Opcion 1: Crear Billetera");
+        Console.WriteLine("Opcion 2: Mostrar una Billetera");
+        Console.WriteLine("Opcion 3: Comprobar total de dinero en una billetera");
+        Console.WriteLine("Opcion 4: Combinar 2 billeteras creando una nueva");
+    
+        opcion = Convert.ToInt32(Console.ReadLine());
+
+    } while (opcion > 0 && opcion < 5);
+
+    switch (opcion)
+    {
+        case 1:
+            Billetera billetera = new Billetera();
+
+
+            break;
+    }
+
+    Billetera billetera1 = new Billetera()
+    {
+        iD = 1,
+        BilletesDe10 = 12,
+        BilletesDe20 = 25,
+        BilletesDe50 = 10,
+        BilletesDe100 = 11,
+        BilletesDe200 = 18,
+        BilletesDe500 = 16,
+        BilletesDe1000 = 5
+    };
+
+    Billetera billetera2 = new Billetera()
+    {
+        iD = 2,
+        BilletesDe10 = 1,
+        BilletesDe20 = 12,
+        BilletesDe50 = 30,
+        BilletesDe100 = 41,
+        BilletesDe200 = 28,
+        BilletesDe500 = 16,
+        BilletesDe1000 = 1
+    };
+
+    Billetera billetera3 = new Billetera()
+    {
+        iD = 3,
+        BilletesDe10 = 2,
+        BilletesDe20 = 5,
+        BilletesDe50 = 18,
+        BilletesDe100 = 31,
+        BilletesDe200 = 13,
+        BilletesDe500 = 26,
+        BilletesDe1000 = 9
+    };
+
+    billeteras.Add(billetera1);
+    billeteras.Add(billetera2);
+    billeteras.Add(billetera3);
+
+
+    foreach (var billetera in billeteras)
+    {
+        billetera.MostrarContenidoDeBilletera();
+        Console.WriteLine();
+    }
+
+
+    Billetera billetera4 = billetera1.Combinar(billetera2);
+    Console.WriteLine();
+
+    billetera4.MostrarContenidoDeBilletera();
+
+    Console.ReadLine();
+
+
+} while (true);
