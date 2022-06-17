542f29c6ca4d3a8fbc7a925be7e73fc303937b4a
Gino Rusconi <ginorusconi@gmail.com>
2022-06-17 15:48:56 -0300

    Agregando clases Cuadrilatero y Trapecio

---
+++ Cuadrilatero/Model/Cuadrilatero.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadrilatero.Model
+{
+    internal abstract class Cuadrilatero
+    {
+        private decimal _x;
+        private decimal _y;
+        private decimal _area;
+
+        #region Setts and Getters
+        public decimal X 
+        {
+            get { return _x; } 
+            set { _x = (value > 0) ? value : 0; }
+        }
+        public decimal Y 
+        {
+            get { return _y; }
+            set { _y = (value > 0) ? value : 0; } 
+        }
+
+        public decimal Area
+        {
+            get { return _area; }
+            set { _area = value; }
+        }
+        #endregion
+
+        public Cuadrilatero()
+        {
+            X = SetMedidas("lado X");
+        }
+
+        protected decimal SetMedidas(String nombreDelLado)
+        {
+            bool checkValue;
+            decimal valorTesteado = 0;
+            do
+            {
+                Console.WriteLine($"Ingrese {nombreDelLado}");
+                checkValue = decimal.TryParse(Console.ReadLine(), out decimal valor);
+                checkValue = (checkValue && valor > 0);
+                if (!checkValue) Console.WriteLine("El valor ingresado no es un nuemero, o es mayor a 0");
+                else valorTesteado = valor;
+
+            } while (!checkValue);
+
+            return valorTesteado;
+        }
+
+        public abstract decimal CalcularArea();
+    }
+}
---
+++ Cuadrilatero/Model/Trapecio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadrilatero.Model
+{
+    internal class Trapecio : Cuadrilatero
+    {
+        private decimal _altura;
+
+        #region Setts and Getters
+        public decimal Altura 
+        {
+            get { return _altura; } 
+            set { _altura = value; } 
+        }
+        #endregion
+
+        public Trapecio() : base()
+        {
+            Console.WriteLine("Ingrese Altura");
+            Y = SetMedidas("lado Y");
+            Altura = SetMedidas("Altura");
+        }
+        
+        public override decimal CalcularArea()
+        {
+            Area = ((X + Y) / 2)* Altura;
+            return Area;
+        }
+    }
+}
