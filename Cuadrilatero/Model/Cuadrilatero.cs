48677dc5fe46b1b970d42c29b573eba5bd1e781b
Gino Rusconi <ginorusconi@gmail.com>
2022-06-17 20:41:51 -0300

    Cambiando Lados por cordenadas

--- Cuadrilatero/Model/Cuadrilatero.cs
+++ Cuadrilatero/Model/Cuadrilatero.cs
@@ -8,51 +8,41 @@ namespace Cuadrilatero.Model
 {
     internal abstract class Cuadrilatero
     {
-        private double _x;
-        private double _y;
-        private double _area;
+
+        private double[,] _Vertices = new double[4,2];
+
+        private double _Area;
 
         #region Setts and Getters
-        public double X 
+        public double[,] Cordenadas
         {
-            get { return _x; } 
-            set { _x = (value > 0) ? value : 0; }
+            get { return _Vertices;}
+            set { _Vertices = value; }
         }
-        public double Y 
-        {
-            get { return _y; }
-            set { _y = (value > 0) ? value : 0; } 
-        }
-
-        public double Area
-        {
-            get { return _area; }
-            set { _area = value; }
+        public double Area 
+        { 
+            get { return _Area;} 
+            set { _Area = value; }
         }
         #endregion
 
         public Cuadrilatero()
         {
-            X = SetMedidas("lado X");
-        }
-
-        protected double SetMedidas(String nombreDelLado)
-        {
-            bool checkValue;
-            double valorTesteado = 0;
-            do
+            for (int Vertice = 0; Vertice < 4; Vertice++)
             {
-                Console.WriteLine($"Ingrese {nombreDelLado}");
-                checkValue = double.TryParse(Console.ReadLine(), out double valor);
-                checkValue = (checkValue && valor > 0);
-                if (!checkValue) Console.WriteLine("El valor ingresado no es un nuemero, o es mayor a 0");
-                else valorTesteado = valor;
-
-            } while (!checkValue);
-
-            return valorTesteado;
+                for (int cordenada = 0; cordenada < 2; cordenada++)
+                {
+                    bool checkValue;
+                    do
+                    {
+                        Console.WriteLine($"Ingrese la cordenada {((cordenada == 0) ? 'x': 'y')} del vertice {Vertice+1}");
+                        checkValue = double.TryParse(Console.ReadLine(), out double valor);
+                        if (!checkValue) Console.WriteLine("El valor ingresado no es un nuemero");
+                        else Cordenadas[Vertice,cordenada] = valor;
+                    } while (!checkValue);
+                }
+            }
         }
-
         public abstract double CalcularArea();
     }
 }
