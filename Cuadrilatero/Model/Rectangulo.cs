65600b1dd9f8e56035a2f8d5c025b6de1183b5a4
Gino Rusconi <ginorusconi@gmail.com>
2022-06-17 16:27:06 -0300

    Agregando la Clase Rectangulo

---
+++ Cuadrilatero/Model/Rectangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuadrilatero.Model
+{
+    internal class Rectangulo : Cuadrilatero
+    {
+        public Rectangulo () : base()
+        {
+            Y = SetMedidas("lado Y"); 
+        }
+
+        public override double CalcularArea()
+        {
+            Area = X * Y;
+            return Area;
+        }
+    }
+}
