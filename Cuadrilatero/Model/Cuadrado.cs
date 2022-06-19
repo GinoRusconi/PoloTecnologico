2382d6cfab08710d35b5b18f18ce7bbfb6c67bdf
Gino Rusconi <ginorusconi@gmail.com>
2022-06-19 19:06:53 -0300

    Cambio de nombre del Proyecto y sus respectivos NameSpace

--- Cuadrilatero/Model/Cuadrado.cs
+++ Cuadrilatero/Model/Cuadrado.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cuadrilatero.Model
+namespace Cuadrilateros.Model
 {
     internal class Cuadrado : Cuadrilatero
     {
